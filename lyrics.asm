055cbcb1f808f502f118f0092f3c7ad731968388
M Dattis <38086897+MssTake@users.noreply.github.com>
2022-11-16 12:14:31 -0500

    lyrics modificaiton

--- lyrics.asm
+++ lyrics.asm
@@ -1,70 +1,210 @@
 INCLUDE Irvine32.inc
 
 .data
-v1l1 BYTE "This is something new", 0
-v1l2 BYTE "The Casper Slide part two", 0
-v1l3 BYTE "Featuring the Platinum Band", 0
-v1l4 BYTE "And this time", 0
-v1l5 BYTE "We're going to get funky", 0
-v1l6 BYTE "Funky", 0
+v1l1 WORD "This is something new", 0
+v1l2 WORD "The Casper Slide part two", 0
+v1l3 WORD "Featuring the Platinum Band", 0
+v1l4 WORD "And this time", 0
+v1l5 WORD "We're going to get funky", 0
+v1l6 WORD "Funky", 0
 
-v2l1 BYTE "Everybody clap your hands", 0
-v2l2 BYTE "Clap, clap, clap, clap your hands", 0
-v2l3 BYTE "Clap, clap, clap, clap your hands", 0
-v2l4 BYTE "Alright now, we're going to do the basic step", 0
+v2l1 WORD "Everybody clap your hands", 0
+v2l2 WORD "Clap, clap, clap, clap your hands", 0
+v2l3 WORD "Clap, clap, clap, clap your hands", 0
+v2l4 WORD "Alright now, we're going to do the basic step", 0
 
-v3l1 BYTE "To the left", 0
-v3l2 BYTE "Take it back now, y'all", 0
-v3l3 BYTE "One hop this time", 0
-v3l4 BYTE "Right foot, let's stomp", 0
-v3l5 BYTE "Left foot, let's stomp", 0
-v3l6 BYTE "Cha cha real smooth", 0
+v3l1 WORD "To the left", 0
+v3l2 WORD "Take it back now, y'all", 0
+v3l3 WORD "One hop this time", 0
+v3l4 WORD "Right foot, let's stomp", 0
+v3l5 WORD "Left foot, let's stomp", 0
+v3l6 WORD "Cha cha real smooth", 0
 
-v4l2 BYTE "Turn it out", 0
-v4l3 BYTE "To the left", 0
-v4l4 BYTE "Take it back now y'all", 0
-v4l5 BYTE "One hop this time", 0
-v4l6 BYTE "Right foot, let's stomp", 0
-v4l7 BYTE "Left foot, let's stomp", 0
-v4l8 BYTE "Cha cha now y'all", 0
-v4l9 BYTE "Now it's time to get funky", 0
+v4l2 WORD "Turn it out", 0
+v4l3 WORD "To the left", 0
+v4l4 WORD "Take it back now y'all", 0
+v4l5 WORD "One hop this time", 0
+v4l6 WORD "Right foot, let's stomp", 0
+v4l7 WORD "Left foot, let's stomp", 0
+v4l8 WORD "Cha cha now y'all", 0
+v4l9 WORD "Now it's time to get funky", 0
 
-v5l1 BYTE "To the right now", 0
-v5l2 BYTE "To the left", 0
-v5l3 BYTE "Take it back now y'all", 0
-v5l4 BYTE "One hop this time", 0
-v5l5 BYTE "One hop this time", 0
-v5l6 BYTE "Right foot two stomps", 0
-v5l7 BYTE "Left foot two stomps", 0
-v5l8 BYTE "Slide to the left", 0
-v5l9 BYTE "Slide to the right", 0
-v5l10 BYTE "Criss cross", 0
-v5l11 BYTE "Criss cross", 0
-v5l12 BYTE "Cha cha real smooth", 0
+v5l1 WORD "To the right now", 0
+v5l2 WORD "To the left", 0
+v5l3 WORD "Take it back now y'all", 0
+v5l4 WORD "One hop this time", 0
+v5l5 WORD "One hop this time", 0
+v5l6 WORD "Right foot two stomps", 0
+v5l7 WORD "Left foot two stomps", 0
+v5l8 WORD "Slide to the left", 0
+v5l9 WORD "Slide to the right", 0
+v5l10 WORD "Criss cross", 0
+v5l11 WORD "Criss cross", 0
+v5l12 WORD "Cha cha real smooth", 0
 
 .code
 main PROC PUBLIC
+	mov dl, 0
+	mov dh, 0
+	call Gotoxy
+
 	v1 PROC
-		mov dl, 0
-		mov dh, 0
-		call Gotoxy
-		mov edx, OFFSET v1l1
+		push edx, OFFSET v1l1
 		call WriteString
 		; figure out how to pause for a bit
-		mov edx, OFFSET v1l2
+		pop edx
+		push edx, OFFSET v1l2
 		call WriteString
+		; figure out how to pause for a bit
+		pop edx
+		push edx, OFFSET v1l3
+		call WriteString
+		; figure out how to pause for a bit
+		pop edx
+		push edx, OFFSET v1l4
+		call WriteString
+		; figure out how to pause for a bit
+		pop edx
+		push edx, OFFSET v1l5
+		call WriteString
+		; figure out how to pause for a bit
+		pop edx
+		push edx, OFFSET v1l6
+		call WriteString
+		pop edx
 	v1 ENDP
 
 	v2 PROC
+		push edx, OFFSET v2l1
+		call WriteString
+		; figure out how to pause for a bit
+		pop edx
+		push edx, OFFSET v2l2
+		call WriteString
+		; figure out how to pause for a bit
+		pop edx
+		push edx, OFFSET v2l3
+		call WriteString
+		; figure out how to pause for a bit
+		pop edx
+		push edx, OFFSET v2l4
+		call WriteString
+		; figure out how to pause for a bit
+		pop edx	
 	v2 ENDP
 
 	v3 PROC
+		push edx, OFFSET v3l1
+		call WriteString
+		; figure out how to pause for a bit
+		pop edx
+		push edx, OFFSET v3l2
+		call WriteString
+		; figure out how to pause for a bit
+		pop edx
+		push edx, OFFSET v3l3
+		call WriteString
+		; figure out how to pause for a bit
+		pop edx
+		push edx, OFFSET v3l4
+		call WriteString
+		; figure out how to pause for a bit
+		pop edx
+		push edx, OFFSET v3l5
+		call WriteString
+		; figure out how to pause for a bit
+		pop edx
+		push edx, OFFSET v3l6
+		call WriteString
+		pop edx
 	v3 ENDP
 
 	v4 PROC
+		push edx, OFFSET v4l1
+		call WriteString
+		; figure out how to pause for a bit
+		pop edx
+		push edx, OFFSET v4l2
+		call WriteString
+		; figure out how to pause for a bit
+		pop edx
+		push edx, OFFSET v4l3
+		call WriteString
+		; figure out how to pause for a bit
+		pop edx
+		push edx, OFFSET v4l4
+		call WriteString
+		; figure out how to pause for a bit
+		pop edx
+		push edx, OFFSET v4l5
+		call WriteString
+		; figure out how to pause for a bit
+		pop edx
+		push edx, OFFSET v4l6
+		call WriteString
+		pop edx
+		push edx, OFFSET v4l7
+		call WriteString
+		; figure out how to pause for a bit
+		pop edx
+		push edx, OFFSET v4l8
+		call WriteString
+		; figure out how to pause for a bit
+		pop edx
+		push edx, OFFSET v4l9
+		call WriteString
+		; figure out how to pause for a bit
+		pop edx
 	v4 ENDP
 
 	v5 PROC
+		push edx, OFFSET v5l1
+		call WriteString
+		; figure out how to pause for a bit
+		pop edx
+		push edx, OFFSET v5l2
+		call WriteString
+		; figure out how to pause for a bit
+		pop edx
+		push edx, OFFSET v5l3
+		call WriteString
+		; figure out how to pause for a bit
+		pop edx
+		push edx, OFFSET v5l4
+		call WriteString
+		; figure out how to pause for a bit
+		pop edx
+		push edx, OFFSET v5l5
+		call WriteString
+		; figure out how to pause for a bit
+		pop edx
+		push edx, OFFSET v5l6
+		call WriteString
+		pop edx
+		push edx, OFFSET v5l7
+		call WriteString
+		; figure out how to pause for a bit
+		pop edx
+		push edx, OFFSET v5l8
+		call WriteString
+		; figure out how to pause for a bit
+		pop edx
+		push edx, OFFSET v5l9
+		call WriteString
+		; figure out how to pause for a bit
+		pop edx
+		push edx, OFFSET v5l10
+		call WriteString
+		; figure out how to pause for a bit
+		pop edx
+		push edx, OFFSET v5l11
+		call WriteString
+		; figure out how to pause for a bit
+		pop edx
+		push edx, OFFSET v5l12
+		call WriteString
+		; figure out how to pause for a bit
+		pop edx
 	v5 ENDP
 
 main ENDP
--- main.asm
+++ main.asm
@@ -3,12 +3,18 @@ INCLUDE lyrics.inc
 INCLUDE controls.inc
 
 .data
-
+score BYTE 0,0
 
 .code
 main PROC
+; get this to play 131 bpm
 
+	score PROC
+	; did the player move on the beat?
 
+		; increase point
+
+	score ENDP
 
 main ENDP
 END main
