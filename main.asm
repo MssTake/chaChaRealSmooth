d29ec29369aa11c603f174570eeff57fa40f2f00
M Dattis <38086897+MssTake@users.noreply.github.com>
2022-11-17 09:52:53 -0500

    please stop crashing please stop crashing please s

--- main.asm
+++ main.asm
@@ -1,106 +1,83 @@
 INCLUDE Irvine32.inc
 
 .data
-score BYTE 0,0
-inputChar BYTE ?
+	score BYTE 0,0
+	inputChar BYTE ?
 
-; controls
-defa BYTE "|",0
-clap BYTE "clap",0
-left BYTE "/",0
-back BYTE ".",0
-oneh BYTE "'",0
-righ BYTE "\",0
-cha1 BYTE "{",0
-cha2 BYTE "}",0
+	; controls
+	defa BYTE "|",0
+	clap BYTE "clap",0
+	left BYTE "/",0
+	back BYTE ".",0
+	oneh BYTE "'",0
+	righ BYTE "\",0
+	cha1 BYTE "{",0
+	cha2 BYTE "}",0
 
-; lyrics
-v1l1 BYTE "This is something new",0
-v1l2 BYTE "The Casper Slide part two",0
-v1l3 BYTE "Featuring the Platinum Band",0
-v1l4 BYTE "And this time",0
-v1l5 BYTE "We're going to get funky",0
-v1l6 BYTE "Funky",0
+	; lyrics
+	v1l1 BYTE "This is something new",0
+	v1l2 BYTE "The Casper Slide part two",0
+	v1l3 BYTE "Featuring the Platinum Band",0
+	v1l4 BYTE "And this time",0
+	v1l5 BYTE "We're going to get funky",0
+	v1l6 BYTE "Funky",0
 
-v2l1 BYTE "Everybody clap your hands (c)",0
-v2l2 BYTE "Clap, clap, clap, clap your hands",0
-v2l3 BYTE "Clap, clap, clap, clap your hands",0
-v2l4 BYTE "Alright now, we're going to do the basic step",0
+	v2l1 BYTE "Everybody clap your hands (c)",0
+	v2l2 BYTE "Clap, clap, clap, clap your hands",0
+	v2l3 BYTE "Clap, clap, clap, clap your hands",0
+	v2l4 BYTE "Alright now, we're going to do the basic step",0
 
-v3l1 BYTE "To the left (a)",0
-v3l2 BYTE "Take it back now, y'all (s)",0
-v3l3 BYTE "One hop this time (w)",0
-v3l4 BYTE "Right foot, let's stomp (d)",0
-v3l5 BYTE "Left foot, let's stomp",0
-v3l6 BYTE "Cha cha real smooth",0
+	v3l1 BYTE "To the left (a)",0
+	v3l2 BYTE "Take it back now, y'all (s)",0
+	v3l3 BYTE "One hop this time (w)",0
+	v3l4 BYTE "Right foot, let's stomp (d)",0
+	v3l5 BYTE "Left foot, let's stomp",0
+	v3l6 BYTE "Cha cha real smooth",0
 
-v4l1 BYTE "Turn it out (I forgot to code this one, sorry)",0
-v4l2 BYTE "To the left",0
-v4l3 BYTE "Take it back now y'all",0
-v4l4 BYTE "One hop this time",0
-v4l5 BYTE "Right foot, let's stomp",0
-v4l6 BYTE "Left foot, let's stomp",0
-v4l7 BYTE "Cha cha now y'all",0
-v4l8 BYTE "Now it's time to get funky",0
-
-v5l1 BYTE "To the right now",0
-v5l2 BYTE "To the left",0
-v5l3 BYTE "Take it back now y'all",0
-v5l4 BYTE "One hop this time",0
-v5l5 BYTE "One hop this time",0
-v5l6 BYTE "Right foot two stomps",0
-v5l7 BYTE "Left foot two stomps",0
-v5l8 BYTE "Slide to the left",0
-v5l9 BYTE "Slide to the right",0
-v5l10 BYTE "Criss cross",0
-v5l11 BYTE "Criss cross",0
-v5l12 BYTE "Cha cha real smooth",0
+	v4l1 BYTE "Turn it out (I forgot to code this one, sorry)",0
+	v4l2 BYTE "To the left",0
+	v4l3 BYTE "Take it back now y'all",0
+	v4l4 BYTE "One hop this time",0
+	v4l5 BYTE "Right foot, let's stomp",0
+	v4l6 BYTE "Left foot, let's stomp",0
+	v4l7 BYTE "Cha cha now y'all",0
+	v4l8 BYTE "Now it's time to get funky",0
 
 .code
 main PROC
-
-	; character appearance
 	mov eax,black (white * 16)
 	call SetTextColor
 	call Clrscr
-
-	; position
-	mov  dl,40  ;column
-	mov  dh,12  ;row
-	call Gotoxy
+	mov dl,40
+	mov dh,12
+	call GotoXY
 
 	; verse 1
 	mov edx,OFFSET v1l1
 	call WriteString
 	mov eax,916
 	call Delay
-	pop edx
 	call Clrscr
 	mov edx,OFFSET v1l2
 	call WriteString
-	pop edx
 	call Clrscr
 	mov edx,OFFSET v1l3
 	call WriteString
 	mov eax,916
 	call Delay
-	pop edx
 	call Clrscr
 	mov edx,OFFSET v1l4
 	call WriteString
 	mov eax,916
 	call Delay
-	pop edx
 	call Clrscr
 	mov edx,OFFSET v1l5
 	call WriteString
 	mov eax,916
 	call Delay
-	pop edx
 	call Clrscr
 	mov edx,OFFSET v1l6
 	call WriteString
-	pop edx
 	call Clrscr
 
 	; verse 2
@@ -109,27 +86,23 @@ main PROC
 	mov eax,916
 	call Delay
 	je didClap
-	pop edx
 	call Clrscr
 	mov edx,OFFSET v2l2
 	call WriteString
 	mov eax,916
 	call Delay
 	je didClap
-	pop edx
 	call Clrscr
 	mov edx,OFFSET v2l3
 	call WriteString
 	mov eax,916
 	call Delay
 	je didClap
-	pop edx
 	call Clrscr
 	mov edx,OFFSET v2l4
 	call WriteString
 	mov eax,916
 	call Delay
-	pop edx	
 	call Clrscr
 
 	;verse 3
@@ -138,35 +111,30 @@ main PROC
 	mov eax,916
 	call Delay
 	je didLeft
-	pop edx
 	call Clrscr
 	mov edx,OFFSET v3l2
 	call WriteString
 	mov eax,916
 	call Delay
 	je didBack
-	pop edx
 	call Clrscr
 	mov edx,OFFSET v3l3
 	call WriteString
 	mov eax,916
 	call Delay
 	je didOneh
-	pop edx
 	call Clrscr
 	mov edx,OFFSET v3l4
 	call WriteString
 	mov eax,916
 	call Delay
 	je didRigh
-	pop edx
 	call Clrscr
 	mov edx,OFFSET v3l5
 	call WriteString
 	mov eax,916
 	call Delay
 	je didLeft
-	pop edx
 	call Clrscr
 	mov edx,OFFSET v3l6
 	call WriteString
@@ -174,7 +142,6 @@ main PROC
 	call Delay
 	je didCha1
 	je didCha2
-	pop edx
 	call Clrscr
 
 	; verse 4
@@ -182,42 +149,36 @@ main PROC
 	call WriteString
 	mov eax,916
 	call Delay
-	pop edx
 	call Clrscr
 	mov edx,OFFSET v4l2
 	call WriteString
 	mov eax,916
 	call Delay
 	je didLeft
-	pop edx
 	call Clrscr
 	mov edx,OFFSET v4l3
 	call WriteString
 	mov eax,916
 	call Delay
 	je didBack
-	pop edx
 	call Clrscr
 	mov edx,OFFSET v4l4
 	call WriteString
 	mov eax,916
 	call Delay
 	je didOneh
-	pop edx
 	call Clrscr
 	mov edx,OFFSET v4l5
 	call WriteString
 	mov eax,916
 	call Delay
 	je didRigh
-	pop edx
 	call Clrscr
 	mov edx,OFFSET v4l6
 	call WriteString
 	mov eax,916
 	call Delay
 	je didLeft
-	pop edx
 	call Clrscr
 	mov edx,OFFSET v4l7
 	call WriteString
@@ -225,14 +186,12 @@ main PROC
 	call Delay
 	je didCha1
 	je didCha2
-	pop edx
 	call Clrscr
 	mov edx,OFFSET v4l8
 	call WriteString
 	mov eax,916
 	call Delay
-	pop edx
-
+	
 	;score
 	call Clrscr
 	mov edx,OFFSET score
@@ -240,88 +199,74 @@ main PROC
 	mov eax,916
 	call Delay
 
-	; character idle and controls
+	; controls
 	didClap:
 		call Clrscr
-		mov  dl,40  ;column
-		mov  dh,12  ;row
 		mov al,defa
 		call WriteChar
+		call ReadChar
 		mov inputChar,al
 		cmp inputChar,"c"
-		call Clrscr
 		inc	score
 		mov al,clap
 		call WriteString
 	didLeft:
 		call Clrscr
-		mov  dl,40  ;column
-		mov  dh,12  ;row
 		mov al,defa
 		call WriteChar
+		call ReadChar
 		mov inputChar,al
 		cmp inputChar,"a"
-		call Clrscr
 		inc score
 		mov al,left
 		call WriteChar
-	didback:
+	didBack:
 		call Clrscr
-		mov  dl,40  ;column
-		mov  dh,12  ;row
 		mov al,defa
 		call WriteChar
+		call ReadChar
 		mov inputChar,al
 		cmp inputChar,"s"
-		call Clrscr
 		inc score
 		mov al,back
 		call WriteChar
 	didOneh:
 		call Clrscr
-		mov  dl,40  ;column
-		mov  dh,12  ;row
 		mov al,defa
 		call WriteChar
+		call ReadChar
 		mov inputChar,al
 		cmp inputChar,"w"
-		call Clrscr
 		inc score
 		mov al,oneh
 		call WriteChar
 	didRigh:
 		call Clrscr
-		mov  dl,40  ;column
-		mov  dh,12  ;row
 		mov al,defa
 		call WriteChar
+		call ReadChar
 		mov inputChar,al
 		cmp inputChar,"d"
-		call Clrscr
 		inc score
 		mov al,righ
 		call WriteChar
 	didCha1:
 		call Clrscr
-		mov  dl,40  ;column
-		mov  dh,12  ;row
 		mov al,defa
 		call WriteChar
+		call ReadChar
 		mov inputChar,al
 		cmp inputChar,"q"
-		call Clrscr
 		inc score
 		mov al,cha1
 		call WriteChar
 	didCha2:
 		call Clrscr
-		mov  dl,40  ;column
-		mov  dh,12  ;row
 		mov al,defa
 		call WriteChar
+		call ReadChar
 		mov inputChar,al
 		cmp inputChar,"e"
-		call Clrscr
 		inc score
 		mov al,cha2
 		call WriteChar
